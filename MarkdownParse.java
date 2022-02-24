e925d89bbf8580b71c64697fa378edb6b4107b01
eunkjm <deu05009@gmail.com>
2022-02-24 12:08:24 -0800

    +1 returned

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -35,7 +35,7 @@ public class MarkdownParse {
                     toReturn.add(markdown.substring(openParen + 1, closeParen));
            }
 
-            currentIndex = closeParen;
+            currentIndex = closeParen + 1;
         }
         return toReturn;
     }
